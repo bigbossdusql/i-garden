77d3a77ad9dc87ea66a7f4abf519ea688824f63b
Mickaëlle Green <84454377+MickaelleGreen@users.noreply.github.com>
2022-04-20 12:15:10 +0200

    Update view_liste_des_plantes.sql

--- view_liste_des_plantes.sql
+++ view_liste_des_plantes.sql
@@ -1,23 +1,12 @@
 USE `i-garden`;
 
+DROP VIEW IF EXISTS `liste_des_plantes`;
 CREATE VIEW `liste_des_plantes` 
 AS 
-SELECT 
--- jointure nom de la plante sur l'id de la variété Graine de l'instance de Plante
-GRAINE.nom
-FROM GRAINE 
-INNER JOIN PLANTE ON GRAINE.id_graine = PLANTE.id_graine
-UNION ALL
+SELECT graine.nom AS 'graine', zone.nom AS 'zone', cycle_vegetatif.nom AS 'cycle végétatif'
+FROM graine
+INNER JOIN plante ON graine.id_graine = plante.id_graine
+LEFT JOIN zone on plante.id_zone = zone.id_zone
+INNER JOIN cycle_vegetatif on plante.id_cycle_vegetatif = cycle_vegetatif.id_cycle_vegetatif;
 
-SELECT
--- jointure nom de la zone via id clé étrangère de Zone
-ZONE.nom
-FROM ZONE
-INNER JOIN PLANTE ON ZONE.id_zone = PLANTE.id_zone
-UNION ALL
-
-SELECT
--- jointure nom du cycle avec clé sur l'id du Cycle
-CYCLE_VEGETATIF.nom
-FROM CYCLE_VEGETATIF
-INNER JOIN PLANTE ON CYCLE_VEGETATIF.id_cycle_vegetatiff = PLANTE.id_cycle_vegetatif
+SELECT * FROM `liste_des_plantes`;
