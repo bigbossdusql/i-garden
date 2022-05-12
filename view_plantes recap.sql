548c9a43a34a11e9c4488d0efa8b689f8ecb1b05
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-05-12 20:22:35 +0200

    Add files via upload

--- view_plantes recap.sql
+++ view_plantes recap.sql
@@ -1,8 +1,14 @@
 USE `i-garden`;
-CREATE VIEW plantes_recap 
-AS
 
+DROP VIEW IF EXISTS `plantes_recap`;
+CREATE VIEW `plantes_recap` 
+AS 
 SELECT 
-graine.nom AS 'nom', graine.classification AS 'class', graine.nom_latin AS 'nom_lat'
+graine.nom, graine.nom_latin, 
+CONCAT(UCASE(SUBSTRING(f1.nom_famille, 1, 1)), LCASE(SUBSTRING(f1.nom_famille, 2))) AS 'Espece', 
+f2.nom_famille AS 'Genre'
 FROM graine
-INNER JOIN graine ON FAMILLE.id_famille = graine.id_famille;
+INNER JOIN famille AS f1 ON f1.id_famille = graine.id_famille
+LEFT OUTER JOIN famille AS f2 ON f2.id_famille_parente = f1.id_famille;
+
+SELECT * FROM `plantes_recap`;
